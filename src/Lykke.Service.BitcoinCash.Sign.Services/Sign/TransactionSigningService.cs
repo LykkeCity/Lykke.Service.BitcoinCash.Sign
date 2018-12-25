ea30f4d40fadc8b9c2ee26c05c477dc26ba0af0f
some-dev <avolkov.dev@gmail.com>
2018-12-26 00:30:36 +0300

    rewrite tx signing modern way

--- src/Lykke.Service.BitcoinCash.Sign.Services/Sign/TransactionSigningService.cs
+++ src/Lykke.Service.BitcoinCash.Sign.Services/Sign/TransactionSigningService.cs
@@ -40,67 +40,16 @@ namespace Lykke.BitcoinCash.Sign.Services.Sign
         {
             var context = Serializer.ToObject<TransactionInfo>(transactionContext);
 
-            var tx = new Transaction(context.TransactionHex);
+            var tx = Transaction.Parse(context.TransactionHex, _network);
 
+            var secretKeys = privateKeys.Select(p => Key.Parse(p, _network)).ToArray();
 
-            var secretKeys = privateKeys.Select(p => Key.Parse(p, _network)).ToList();
+            var signed = _network.CreateTransactionBuilder()
+                .AddCoins(context.UsedCoins)
+                .AddKeys(secretKeys)
+                .SignTransaction(tx);
 
-            Key GetPrivateKey(TxDestination pubKeyHash)
-            {
-                foreach (var secret in secretKeys)
-                {
-                    var key = new BitcoinSecret(secret, _network);
-                    if (key.PubKey.Hash == pubKeyHash)
-                        return key.PrivateKey;
-                }
-
-                return null;
-            }
-            
-            const SigHash hashType = SigHash.All;
-
-            for (int i = 0; i < tx.Inputs.Count; i++)
-            {
-                var input = tx.Inputs[i];
-
-                var output = context.UsedCoins.FirstOrDefault(o => o.Outpoint == input.PrevOut)?.TxOut;
-
-                if (output == null)
-                    throw new BusinessException("Input not found", ErrorCode.InputNotFound);
-
-                if (PayToPubkeyHashTemplate.Instance.CheckScriptPubKey(output.ScriptPubKey))
-                {
-                    var secret = GetPrivateKey(PayToPubkeyHashTemplate.Instance.ExtractScriptPubKeyParameters(output.ScriptPubKey));
-                    if (secret != null)
-                    {
-                        var hash = Script.SignatureHash(output.ScriptPubKey, tx, i, hashType, output.Value);
-                        var signature = secret.Sign(hash, hashType);
-
-                        tx.Inputs[i].ScriptSig = PayToPubkeyHashTemplate.Instance.GenerateScriptSig(signature, secret.PubKey);
-                        continue;
-                    }
-                    throw new BusinessException("Incompatible private key", ErrorCode.IncompatiblePrivateKey);
-                }
-
-                if (PayToPubkeyTemplate.Instance.CheckScriptPubKey(output.ScriptPubKey))
-                {
-                    var secret = GetPrivateKey(PayToPubkeyTemplate.Instance.ExtractScriptPubKeyParameters(output.ScriptPubKey).Hash);
-                    if (secret != null)
-                    {
-                        var hash = Script.SignatureHash(output.ScriptPubKey, tx, i, hashType, output.Value);
-                        var signature = secret.Sign(hash, hashType);
-
-                        tx.Inputs[i].ScriptSig = PayToPubkeyTemplate.Instance.GenerateScriptSig(signature);
-                        continue;
-                    }
-                    throw new BusinessException("Incompatible private key", ErrorCode.InvalidScript);
-                }
-
-
-                throw new BusinessException("Incompatible private key", ErrorCode.InvalidScript);
-            }
-
-            return SignResult.Ok(tx.ToHex());
+            return SignResult.Ok(signed.ToHex());
         }
     }
 }
